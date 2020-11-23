91afcdb1f70f24a386c90dcbe9865a0173f33bfd
sergeynagorny <sergey.s.nagorny@gmail.com>
2020-11-23 22:15:19 +0300

    Рендеринг ключевых компонентов

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+const TASK_COUNT = 3;
+
 const createSiteMenuTemplate = () => {
   return (
     `<section class="control__btn-wrap">
@@ -163,99 +165,95 @@ const createTaskTemplate = () => {
 
 const createTaskEditTemplate = () => {
   return (
-    `<article class="card card--edit card--black">
+    `<article class="card card--edit card--yellow card--repeat">
       <form class="card__form" method="get">
         <div class="card__inner">
           <div class="card__color-bar">
-            <svg width="100%" height="10">
+            <svg class="card__color-bar-wave" width="100%" height="10">
               <use xlink:href="#wave"></use>
             </svg>
           </div>
-
           <div class="card__textarea-wrap">
             <label>
               <textarea
                 class="card__text"
                 placeholder="Start typing your text here..."
                 name="text"
-              >This is example of new task, you can set date and time.</textarea>
+              >Here is a card with filled data</textarea>
             </label>
           </div>
-
           <div class="card__settings">
             <div class="card__details">
               <div class="card__dates">
                 <button class="card__date-deadline-toggle" type="button">
-                  date: <span class="card__date-status">no</span>
+                  date: <span class="card__date-status">yes</span>
                 </button>
-
-                <fieldset class="card__date-deadline" disabled>
+                <fieldset class="card__date-deadline">
                   <label class="card__input-deadline-wrap">
                     <input
                       class="card__date"
                       type="text"
-                      placeholder="23 September"
+                      placeholder=""
                       name="date"
+                      value="23 September 16:15"
                     />
                   </label>
                 </fieldset>
-
                 <button class="card__repeat-toggle" type="button">
-                  repeat:<span class="card__repeat-status">no</span>
+                  repeat:<span class="card__repeat-status">yes</span>
                 </button>
-
-                <fieldset class="card__repeat-days" disabled>
+                <fieldset class="card__repeat-days">
                   <div class="card__repeat-days-inner">
                     <input
                       class="visually-hidden card__repeat-day-input"
                       type="checkbox"
-                      id="repeat-mo-1"
+                      id="repeat-mo-4"
                       name="repeat"
                       value="mo"
                     />
-                    <label class="card__repeat-day" for="repeat-mo-1"
+                    <label class="card__repeat-day" for="repeat-mo-4"
                       >mo</label
                     >
                     <input
                       class="visually-hidden card__repeat-day-input"
                       type="checkbox"
-                      id="repeat-tu-1"
+                      id="repeat-tu-4"
                       name="repeat"
                       value="tu"
                       checked
                     />
-                    <label class="card__repeat-day" for="repeat-tu-1"
+                    <label class="card__repeat-day" for="repeat-tu-4"
                       >tu</label
                     >
                     <input
                       class="visually-hidden card__repeat-day-input"
                       type="checkbox"
-                      id="repeat-we-1"
+                      id="repeat-we-4"
                       name="repeat"
                       value="we"
                     />
-                    <label class="card__repeat-day" for="repeat-we-1"
+                    <label class="card__repeat-day" for="repeat-we-4"
                       >we</label
                     >
                     <input
                       class="visually-hidden card__repeat-day-input"
                       type="checkbox"
-                      id="repeat-th-1"
+                      id="repeat-th-4"
                       name="repeat"
                       value="th"
                     />
-                    <label class="card__repeat-day" for="repeat-th-1"
+                    <label class="card__repeat-day" for="repeat-th-4"
                       >th</label
                     >
                     <input
                       class="visually-hidden card__repeat-day-input"
                       type="checkbox"
-                      id="repeat-fr-1"
+                      id="repeat-fr-4"
                       name="repeat"
                       value="fr"
                       checked
                     />
-                    <label class="card__repeat-day" for="repeat-fr-1"
+                    <label class="card__repeat-day" for="repeat-fr-4"
                       >fr</label
                     >
                     <input
@@ -263,95 +261,93 @@ const createTaskEditTemplate = () => {
                       type="checkbox"
                       name="repeat"
                       value="sa"
-                      id="repeat-sa-1"
+                      id="repeat-sa-4"
                     />
-                    <label class="card__repeat-day" for="repeat-sa-1"
+                    <label class="card__repeat-day" for="repeat-sa-4"
                       >sa</label
                     >
                     <input
                       class="visually-hidden card__repeat-day-input"
                       type="checkbox"
-                      id="repeat-su-1"
+                      id="repeat-su-4"
                       name="repeat"
                       value="su"
                       checked
                     />
-                    <label class="card__repeat-day" for="repeat-su-1"
+                    <label class="card__repeat-day" for="repeat-su-4"
                       >su</label
                     >
                   </div>
                 </fieldset>
               </div>
             </div>
-
             <div class="card__colors-inner">
               <h3 class="card__colors-title">Color</h3>
               <div class="card__colors-wrap">
                 <input
                   type="radio"
-                  id="color-black-1"
+                  id="color-black-4"
                   class="card__color-input card__color-input--black visually-hidden"
                   name="color"
                   value="black"
-                  checked
                 />
                 <label
-                  for="color-black-1"
+                  for="color-black-4"
                   class="card__color card__color--black"
                   >black</label
                 >
                 <input
                   type="radio"
-                  id="color-yellow-1"
+                  id="color-yellow-4"
                   class="card__color-input card__color-input--yellow visually-hidden"
                   name="color"
                   value="yellow"
+                  checked
                 />
                 <label
-                  for="color-yellow-1"
+                  for="color-yellow-4"
                   class="card__color card__color--yellow"
                   >yellow</label
                 >
                 <input
                   type="radio"
-                  id="color-blue-1"
+                  id="color-blue-4"
                   class="card__color-input card__color-input--blue visually-hidden"
                   name="color"
                   value="blue"
                 />
                 <label
-                  for="color-blue-1"
+                  for="color-blue-4"
                   class="card__color card__color--blue"
                   >blue</label
                 >
                 <input
                   type="radio"
-                  id="color-green-1"
+                  id="color-green-4"
                   class="card__color-input card__color-input--green visually-hidden"
                   name="color"
                   value="green"
                 />
                 <label
-                  for="color-green-1"
+                  for="color-green-4"
                   class="card__color card__color--green"
                   >green</label
                 >
                 <input
                   type="radio"
-                  id="color-pink-1"
+                  id="color-pink-4"
                   class="card__color-input card__color-input--pink visually-hidden"
                   name="color"
                   value="pink"
                 />
                 <label
-                  for="color-pink-1"
+                  for="color-pink-4"
                   class="card__color card__color--pink"
                   >pink</label
                 >
               </div>
             </div>
           </div>
-
           <div class="card__status-btns">
             <button class="card__save" type="submit">save</button>
             <button class="card__delete" type="button">delete</button>
@@ -369,3 +365,19 @@ const createLoadMoreButtonTemplate = () => {
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
 };
+
+const siteMainElement = document.querySelector(`.main`);
+const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
+
+render(siteHeaderElement, createSiteMenuTemplate());
+render(siteMainElement, createFilterTemplate());
+render(siteMainElement, createBoardTemplate());
+const boardElement = document.querySelector(`.board`);
+render(boardElement, createSortTemplate());
+render(boardElement, createTaskBoardTemplate());
+const taskListElement = document.querySelector(`.board__tasks`);
+render(taskListElement, createTaskEditTemplate());
+for (let i = 0; i < TASK_COUNT; i++) {
+  render(taskListElement, createTaskTemplate());
+}
+render(boardElement, createLoadMoreButtonTemplate());
