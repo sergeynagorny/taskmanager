0a63468fb0c71524d30ab452a93b3f22d4471cbe
sergeynagorny <sergey.s.nagorny@gmail.com>
2020-11-26 15:17:55 +0300

    Генерация формы редактирования задачи и карточки задачи

--- src/main.js
+++ src/main.js
@@ -7,10 +7,13 @@ import {createLoadMoreButtonTemplate} from "./view/load-more-button.js";
 import {createBoardTemplate} from "./view/board.js";
 import {createTaskBoardTemplate} from "./view/task-board.js";
 import {generateFilters} from "./model/filter.js";
+import {generateTasks} from "./model/task.js";
 
 
-const TASK_COUNT = 3;
+const TASK_COUNT = 22;
+
 const dataFilters = generateFilters();
+const dataTasks = generateTasks(TASK_COUNT);
 
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
@@ -26,8 +29,8 @@ const boardElement = document.querySelector(`.board`);
 render(boardElement, createSortTemplate());
 render(boardElement, createTaskBoardTemplate());
 const taskListElement = document.querySelector(`.board__tasks`);
-render(taskListElement, createTaskEditTemplate());
-for (let i = 0; i < TASK_COUNT; i++) {
-  render(taskListElement, createTaskTemplate());
+render(taskListElement, createTaskEditTemplate(dataTasks[0]));
+for (let i = 1; i < dataTasks.length; i++) {
+  render(taskListElement, createTaskTemplate(dataTasks[i]));
 }
 render(boardElement, createLoadMoreButtonTemplate());
--- src/view/filter.js
+++ src/view/filter.js
@@ -1,4 +1,4 @@
-const createFilterMarckup = (filter, isChecked) => {
+const createFilterMarkup = (filter, isChecked) => {
   const {name, count} = filter;
 
   return (
@@ -8,7 +8,7 @@ const createFilterMarckup = (filter, isChecked) => {
 };
 
 export const createFilterTemplate = (filters) => {
-  const filtersMarkup = filters.map((it, i) => createFilterMarckup(it, i === 0)).join(`\n`);
+  const filtersMarkup = filters.map((it, i) => createFilterMarkup(it, i === 0)).join(`\n`);
 
   return (
     `<section class="main__filter filter container">
@@ -16,63 +16,3 @@ export const createFilterTemplate = (filters) => {
     </section>`
   );
 };
-
-// `<section class="main__filter filter container">
-//       <input
-//         type="radio"
-//         id="filter__all"
-//         class="filter__input visually-hidden"
-//         name="filter"
-//         checked
-//       />
-//       <label for="filter__all" class="filter__label">
-//         All <span class="filter__all-count">13</span></label
-//       >
-//       <input
-//         type="radio"
-//         id="filter__overdue"
-//         class="filter__input visually-hidden"
-//         name="filter"
-//         disabled
-//       />
-//       <label for="filter__overdue" class="filter__label"
-//         >Overdue <span class="filter__overdue-count">0</span></label
-//       >
-//       <input
-//         type="radio"
-//         id="filter__today"
-//         class="filter__input visually-hidden"
-//         name="filter"
-//         disabled
-//       />
-//       <label for="filter__today" class="filter__label"
-//         >Today <span class="filter__today-count">0</span></label
-//       >
-//       <input
-//         type="radio"
-//         id="filter__favorites"
-//         class="filter__input visually-hidden"
-//         name="filter"
-//       />
-//       <label for="filter__favorites" class="filter__label"
-//         >Favorites <span class="filter__favorites-count">1</span></label
-//       >
-//       <input
-//         type="radio"
-//         id="filter__repeating"
-//         class="filter__input visually-hidden"
-//         name="filter"
-//       />
-//       <label for="filter__repeating" class="filter__label"
-//         >Repeating <span class="filter__repeating-count">1</span></label
-//       >
-//       <input
-//         type="radio"
-//         id="filter__archive"
-//         class="filter__input visually-hidden"
-//         name="filter"
-//       />
-//       <label for="filter__archive" class="filter__label"
-//         >Archive <span class="filter__archive-count">115</span></label
-//       >
-//     </section>`;
