c1c0fcaf9e6a5395aaca5efd6600c98e386c7fd3
sergeynagorny <sergey.s.nagorny@gmail.com>
2021-01-04 09:56:27 +0300

    Рефакторинг API

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import TasksModel from "./model/tasks.js";
 import {render} from "./utils/render.js";
 
 const AUTHORIZATION = `Basic Asdhjasdj22`;
+const END_POINT = `https://11.ecmascript.pages.academy/task-manager`;
 
 const dateTo = new Date();
 const dateFrom = (() => {
@@ -17,7 +18,7 @@ const dateFrom = (() => {
   return d;
 })();
 
-const api = new API(AUTHORIZATION);
+const api = new API(END_POINT, AUTHORIZATION);
 const tasksModel = new TasksModel();
 
 const siteMainElement = document.querySelector(`.main`);
