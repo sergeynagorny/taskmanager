1deb9a9f8823c330f087564661dd49e8b73679c7
sergeynagorny <sergey.s.nagorny@gmail.com>
2021-01-04 10:02:04 +0300

    Реализация создания данных на сервере

--- src/api.js
+++ src/api.js
@@ -27,6 +27,17 @@ export default class API {
       .then(Task.parseTasks);
   }
 
+  createTask(task) {
+    return this._load({
+      url: `tasks`,
+      method: Method.POST,
+      body: JSON.stringify(task.toRAW()),
+      headers: new Headers({"Content-Type": `application/json`})
+    })
+      .then((response) => response.json())
+      .then(Task.parseTask);
+  }
+
   updateTask(id, data) {
     return this._load({
       url: `tasks/${id}`,
